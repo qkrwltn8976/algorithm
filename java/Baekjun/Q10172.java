188caea218e8f136619c5f1236278d59d9ec0aa5
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2021-08-02 21:10:15 +0900

    입출력 및 사칙연산

---
+++ java/Baekjun/Q10172.java
@@ -0,0 +1,19 @@
+package Baekjun;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Q10172 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        str = "|\\_/|" + "\n"
+            + "|q p|   /}" + "\n"
+            + "( 0 )\"\"\"\\" + "\n"
+            + "|\"^\"`    |" + "\n"
+            + "||_/=\\\\__|";
+        System.out.println(str);
+    }
+}
