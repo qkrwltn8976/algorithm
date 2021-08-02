87e3b412f6918f70456afc03790a31ec90b38d09
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2021-08-02 20:55:54 +0900

    입출력 및 사칙연산

---
+++ java/Baekjun/Q1001.java
@@ -0,0 +1,19 @@
+package Baekjun;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a-b);
+    }
+}
---
+++ java/Baekjun/Q1008.java
@@ -0,0 +1,19 @@
+package Baekjun;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Q1008 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        
+        int a = Integer.parseInt(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+
+        System.out.println(a/b);
+    }
+}
