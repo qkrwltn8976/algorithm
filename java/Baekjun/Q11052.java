60846f945d6e0a2eeae3b6a55f88bbcbf12f2fd2
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2021-10-24 18:16:41 +0900

    dp

---
+++ java/Baekjun/Q11052.java
@@ -0,0 +1,29 @@
+package Baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q11052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 민규가 구매하려고 하는 카드의 개수
+        int[] arr = new int[N+1]; // 카드가 i개 포함된 카드팩의 가격은 Pi원
+        int[] dp = new int[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i=1; i<=N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=i; j++) {
+                // i-j개 구하는 최대가격에서 j개 사기위한 비용을 더한다.
+                dp[i] = Math.max(arr[j] + dp[i-j], dp[i]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
