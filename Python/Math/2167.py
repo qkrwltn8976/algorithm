cd3b94772818e0b3b363f6265bc5d425418a79af
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-03-19 22:33:05 +0900

    sum

---
+++ Python/Math/2167.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+d = []
+dp = [[0] * (m+1) for _ in range(n+1)]
+for i in range(n):
+    d.append(list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = d[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
+k = int(input())
+
+for _ in range(k):
+    i, j, x, y = map(int, input().split())
+    print(dp[x][y]-dp[x][j-1]-dp[i-1][y]+dp[i-1][j-1])
