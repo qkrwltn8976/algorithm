d5d97dd064a5c12d27b9b03c21739f29cf02d97c
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-04-07 19:43:17 +0900

    divide and conquer
    쿼드트리

--- Python/Divide and Conquer/1992.py
+++ Python/Divide and Conquer/1992.py
@@ -3,31 +3,24 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-d = [list(map(int, input().split())) for _ in range(n)]
-white = 0
-blue = 0
+d = [list(map(int, input().rstrip())) for _ in range(n)]
 
 def dnc(x, y, n):
-    global white
-    global blue
     check = d[x][y]
-    
     for i in range(x, x+n):
         for j in range(y, y+n):
             if check != d[i][j]:
                 check = -1
                 break
-            
     if check == -1:
+        print("(", end="")
         n = n // 2
-        dnc(x, y, n)
-        dnc(x, y + n, n)
-        dnc(x + n, y, n)
-        dnc(x+n, y+n, n)
-    elif check == 1:
-        blue += 1
+        dnc(x, y, n) # 오른쪽 위
+        dnc(x, y+n, n) # 왼쪽 위
+        dnc(x+n, y, n) # 오른쪽 아래
+        dnc(x+n, y+n, n) # 왼쪽 아래
+        print(")", end="")
     else:
-        white += 1
+        print(check, end="")
 
 dnc(0, 0, n)
-print(white, blue)
